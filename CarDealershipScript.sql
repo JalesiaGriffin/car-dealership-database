69cb79fdc06bb4166ff4437db21ea697dec06c15
JalesiaGriffin <jgriffin@appdev.yearup.org>
2023-12-08 11:20:08 -0500

    created inventory table

--- CarDealershipScript.sql
+++ CarDealershipScript.sql
@@ -27,16 +27,28 @@ CREATE INDEX `name` ON `dealerships` (`name`);
 
 
 # ---------------------------------------------------------------------- #
-# Table 2: "Vehicles"                                                    #
+# Table 2: "vehicles"                                                    #
 # ---------------------------------------------------------------------- #
-CREATE TABLE `Vehicles` (
+CREATE TABLE `vehicles` (
     `vin` INTEGER NOT NULL,
-    `year` VARCHAR(4) NOT NULL,
-    `make` VARCHAR(10),
-    `model` VARCHAR(10),
-    `type` VARCHAR(10),
+    `year` INTEGER NOT NULL,
+    `make` VARCHAR(10) NOT NULL,
+    `model` VARCHAR(10) NOT NULL,
+    `type` VARCHAR(10) NOT NULL,
     `color` VARCHAR(10),
     `odometer` VARCHAR(20),
     `price` VARCHAR(20),
     CONSTRAINT `PK_VIN` PRIMARY KEY (`vin`)
+);
+
+
+# ---------------------------------------------------------------------- #
+# Table 3: "inventory"                                                    #
+# ---------------------------------------------------------------------- #
+CREATE TABLE `inventory` (
+    `dealership_id` INTEGER NOT NULL,
+    `vin` INTEGER NOT NULL,
+    CONSTRAINT `PK_VIN` PRIMARY KEY (`vin`),
+    FOREIGN KEY (`dealership_id`) REFERENCES `dealerships`(`dealership_id`),
+    FOREIGN KEY (`vin`) REFERENCES `vehicles`(`vin`)
 );
